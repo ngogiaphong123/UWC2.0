6ed185d9426e2d88e29e91abf5cf8e960cadd577
Kawasaki <iduchungho@gmail.com>
2022-12-07 11:10:47 +0700

    chỉnh sửa route

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './component/UI/Navbar';
 import Home from './component/modules/Home';
 import Dashboard from './component/modules/Dashboard';
-import ListEmployee from './component/modules/ListEmployee';
 
 function App() {
   return (
@@ -13,9 +12,7 @@ function App() {
         <NavbarComponent />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/Dashboard/*' element={<Dashboard />}>
-            <Route path = 'DSNV' element = {<ListEmployee />} />
-          </Route>
+          <Route path='/Dashboard/*' element={<Dashboard />}/>
         </Routes>
       </Router>
     </>

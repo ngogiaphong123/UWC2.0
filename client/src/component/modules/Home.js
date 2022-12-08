1a4fb1c79c26c08d727c67ebb08828d8c59070dd
Kawasaki <iduchungho@gmail.com>
2022-12-08 15:17:54 +0700

    update home.js

--- client/src/component/modules/Home.js
+++ client/src/component/modules/Home.js
@@ -1,9 +1,19 @@
-import React from "react";
+import './styles/Home.css'
 
-export default function Home() {
+import Button from 'react-bootstrap/Button';
+// import Carousel from 'react-bootstrap/Carousel';
+
+function Home() {
     return (
-        <div>
-            Home
+        <div className='home-page'>
+            <div className='title'>
+                <h1>URBAN WASTE COLLECTION AID - UWC 2.0</h1>
+                <Button className='btn-title' href='/login'>
+                    Get Started
+                </Button>
+            </div>
         </div>
-    );
+    )
 }
+
+export default Home;

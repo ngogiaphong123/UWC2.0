ae26e97864577a4444810d3b5f9af177f1a84803
Nguyenleminhbao-tt5 <bao.nguyenminhbaott5@hcmut.edu.vn>
2022-12-12 10:10:07 +0700

    update maps

--- client/src/component/modules/Dashboard.js
+++ client/src/component/modules/Dashboard.js
@@ -3,6 +3,8 @@ import {  Routes, Route, useLocation } from 'react-router-dom'
 import Sidebar from "../UI/Sidebar";
 import './styles/Dashboard.css';
 import ListEmployee from "./ListEmployee";
+import Task from "./Task";
+
 export default function Dashboard(){
     let location = useLocation();
     console.log(location);
@@ -14,6 +16,7 @@ export default function Dashboard(){
             <div className="element">
                 <Routes>
                     <Route path = 'DSNV' element = {<ListEmployee/>}/>
+                    <Route path = 'NV' element = {<Task/>}/>
                 </Routes>
             </div>
         </div>

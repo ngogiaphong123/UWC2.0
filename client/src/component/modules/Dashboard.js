6d69d124915d0e2b1a22f292726d4edab79b7c56
Nguyenleminhbao-tt5 <bao.nguyenminhbaott5@hcmut.edu.vn>
2022-12-12 10:17:18 +0700

    update maps 4

--- client/src/component/modules/Dashboard.js
+++ client/src/component/modules/Dashboard.js
@@ -3,14 +3,11 @@ import {  Routes, Route, useLocation } from 'react-router-dom'
 import Sidebar from "../UI/Sidebar";
 import './styles/Dashboard.css';
 import ListEmployee from "./ListEmployee";
-<<<<<<< HEAD
 import Task from "./Task";
 
-=======
 import Notification from "./Notification";
 import CollectorCalendar from "./CollectorCalendar";
 import JanitorCalendar from "./JanitorCalendar";
->>>>>>> 8b60b26adea77dd82db1a0bbd6acc79164a9efa4
 export default function Dashboard(){
     let location = useLocation();
     // console.log(location);
@@ -23,14 +20,11 @@ export default function Dashboard(){
                 <Routes>
                     <Route path='' element={<ListEmployee />} />
                     <Route path = 'DSNV' element = {<ListEmployee/>}/>
-<<<<<<< HEAD
                     <Route path = 'NV' element = {<Task/>}/>
-=======
                     <Route path = 'DSNV/collectorCalendar' element = {<CollectorCalendar/>}/>
                     <Route path = 'DSNV/janitorCalendar' element = {<JanitorCalendar/>}/>
                     <Route path = 'TB' element = {<Notification/>}/>
                     {/* <Route path = 'NV' element = /> */}
->>>>>>> 8b60b26adea77dd82db1a0bbd6acc79164a9efa4
                 </Routes>
             </div>
         </div>
